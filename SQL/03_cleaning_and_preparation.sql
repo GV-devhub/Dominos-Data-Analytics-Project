6d26676b198276276acb0b4f40d3f66eecc8d89e
GV-devhub <Gayav23@outlook.com>
2025-12-30 22:14:40 -0500

    03_cleaning_and_preparation.sql

--- SQL/03_cleaning_and_preparation.sql
+++ SQL/03_cleaning_and_preparation.sql
@@ -13,3 +13,41 @@ BEGIN
     END IF;
 END;
 /
+-- Normalize Small sizes
+UPDATE pizza_orders
+SET pizza_size = 'Small'
+WHERE LOWER(pizza_size) IN ('s', 'small', 'sm');
+
+-- Normalize Medium sizes
+UPDATE pizza_orders
+SET pizza_size = 'Medium'
+WHERE LOWER(pizza_size) IN ('m', 'medium', 'med');
+
+-- Normalize Large sizes
+UPDATE pizza_orders
+SET pizza_size = 'Large'
+WHERE LOWER(pizza_size) IN ('l', 'large', 'lg');
+
+COMMIT;
+
+UPDATE pizza_orders
+SET pizza_category = 'Veg'
+WHERE LOWER(pizza_category) IN ('veg', 'vegetarian');
+
+UPDATE pizza_orders
+SET pizza_category = 'Non-Veg'
+WHERE LOWER(pizza_category) IN ('nonveg', 'non-veg', 'meat');
+
+--Fix missing total_price values
+UPDATE pizza_orders
+SET total_price = quantity * unit_price
+WHERE total_price IS NULL;
+
+--Convert order_date + order_time into proper Oracle DATE
+ALTER TABLE pizza_orders ADD order_datetime DATE;
+
+UPDATE pizza_orders
+SET order_datetime =
+    TO_DATE(order_date || ' ' || order_time, 'YYYY-MM-DD HH24:MI:SS');
+
+
