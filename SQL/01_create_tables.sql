4b848918b78b03a11ee994bf2ae5882a0068f1f7
GV-devhub <Gayav23@outlook.com>
2025-12-30 22:02:00 -0500

    01_create_tables.sql

--- SQL/01_create_tables.sql
+++ SQL/01_create_tables.sql
@@ -2,13 +2,14 @@
 CREATE TABLE pizza_orders (
     pizza_id        NUMBER,
     order_id        NUMBER,
+    pizza_name_id  VARCHAR2(100),
     quantity        NUMBER,
     order_date      DATE,
     order_time      VARCHAR2(20),
     unit_price      NUMBER(10,2),
     total_price     NUMBER(10,2),
     pizza_size      VARCHAR2(20),
-    pizza_cate      VARCHAR2(50),
-    pizza_ingr      VARCHAR2(500),
+    pizza_category      VARCHAR2(50),
+    pizza_ingredients      VARCHAR2(500),
     pizza_name      VARCHAR2(100)
 );
